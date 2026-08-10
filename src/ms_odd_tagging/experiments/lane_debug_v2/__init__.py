d7d82cc072818f44a1f36c8bbb98e1bdc2a083f9
Olive Park <101008862+livmeraki@users.noreply.github.com>
2026-08-10 17:08:47 +0900

    Bump lane debug version for smoothed inferred union

--- src/ms_odd_tagging/experiments/lane_debug_v2/__init__.py
+++ src/ms_odd_tagging/experiments/lane_debug_v2/__init__.py
@@ -1,3 +1,3 @@
 """Isolated lane-debug v2 experiment."""
 
-DEBUG_IMPLEMENTATION_VERSION = "lane-debug-v2.11-longitudinal-inferred-affiliation"
+DEBUG_IMPLEMENTATION_VERSION = "lane-debug-v2.12-smoothed-inferred-box-union"
