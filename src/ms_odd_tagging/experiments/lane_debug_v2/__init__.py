f964b7b5b643b4ef7705b08b88857f24a828d23a
Olive Park <101008862+livmeraki@users.noreply.github.com>
2026-08-11 18:59:46 +0900

    Bump lane change hysteresis version

--- src/ms_odd_tagging/experiments/lane_debug_v2/__init__.py
+++ src/ms_odd_tagging/experiments/lane_debug_v2/__init__.py
@@ -1,3 +1,3 @@
 """Isolated lane-debug v2 experiment."""
 
-DEBUG_IMPLEMENTATION_VERSION = "lane-debug-v2.27-ranked-inferred-affiliation"
+DEBUG_IMPLEMENTATION_VERSION = "lane-debug-v2.28-lane-change-hysteresis"
