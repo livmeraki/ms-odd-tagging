361514640e3184bf0d75631a3a8ae8d03d4ad0dd
Olive Park <101008862+livmeraki@users.noreply.github.com>
2026-08-20 17:42:16 +0900

    Remove obsolete input_generator compatibility package

--- src/ms_odd_tagging/input_generator/_frame_input_standard_impl.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility alias; implementation moved to :mod:`ms_odd_tagging.frame_inputs._standard_impl`."""
-
-import sys
-
-from ms_odd_tagging.frame_inputs import _standard_impl as _implementation
-
-sys.modules[__name__] = _implementation
