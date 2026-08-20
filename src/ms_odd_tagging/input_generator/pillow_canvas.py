d86c16249325d54be8d69fbd5d0362dd0e065c77
Olive Park <101008862+livmeraki@users.noreply.github.com>
2026-08-20 17:43:56 +0900

    Remove obsolete input_generator compatibility package

--- src/ms_odd_tagging/input_generator/pillow_canvas.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility alias; implementation moved to :mod:`ms_odd_tagging.frame_inputs.pillow_canvas`."""
-
-import sys
-
-from ms_odd_tagging.frame_inputs import pillow_canvas as _implementation
-
-sys.modules[__name__] = _implementation
