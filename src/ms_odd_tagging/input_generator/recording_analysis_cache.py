da34a8aa1fe42d3d8c9978d8c32f515f59fd64d1
Olive Park <101008862+livmeraki@users.noreply.github.com>
2026-08-20 17:44:12 +0900

    Remove obsolete input_generator compatibility package

--- src/ms_odd_tagging/input_generator/recording_analysis_cache.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility alias; implementation moved to :mod:`ms_odd_tagging.frame_inputs.recording_analysis_cache`."""
-
-import sys
-
-from ms_odd_tagging.frame_inputs import recording_analysis_cache as _implementation
-
-sys.modules[__name__] = _implementation
