fe09d43941b64d3b9f97cd6b26ca82f2bb470655
Olive Park <101008862+livmeraki@users.noreply.github.com>
2026-08-20 17:45:55 +0900

    Remove unused Lanelet2 PoC

--- src/ms_odd_tagging/lanelet2_poc/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Optional Lanelet2 proof of concept for local ego-lane identification."""
-
-from .config import DEFAULT_CONFIG, load_config
-from .runner import run_frame, run_recording
-
-__all__ = ["DEFAULT_CONFIG", "load_config", "run_frame", "run_recording"]
