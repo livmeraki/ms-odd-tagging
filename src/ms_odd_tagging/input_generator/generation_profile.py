bf2de3227f869861d43ee698fcddc8a5268de10a
Olive Park <101008862+livmeraki@users.noreply.github.com>
2026-08-20 17:43:35 +0900

    Remove obsolete input_generator compatibility package

--- src/ms_odd_tagging/input_generator/generation_profile.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility alias; implementation moved to :mod:`ms_odd_tagging.frame_inputs.generation_profile`."""
-
-import sys
-
-from ms_odd_tagging.frame_inputs import generation_profile as _implementation
-
-sys.modules[__name__] = _implementation
